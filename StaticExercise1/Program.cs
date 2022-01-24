81fdd89064ab9d884b3f712d9d026ab36ae26c03
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-01-24 11:54:57 -0500

    Add project files.

---
+++ StaticExercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celsius {celsius}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+
+    }
+}
